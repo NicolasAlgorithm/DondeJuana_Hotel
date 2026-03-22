f37651d8818c251f762ff66d78a18f40c0987e3f
Nicolas <ntrujilloc@ucentral.edu.co>
2026-03-22 16:49:49 -0500

    user recepcionista is now available to log in in the system

--- src/test/java/com/project/hotel/GenHash.java
+++ src/test/java/com/project/hotel/GenHash.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class GenHash {
     public static void main(String[] args) {
-        String raw = "admin123";
+        String raw = "12345";
         System.out.println("RAW=[" + raw + "]");
         System.out.println(new BCryptPasswordEncoder().encode(raw));
     }
